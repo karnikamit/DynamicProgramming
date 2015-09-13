9b5581864dcb29c4d33fdc7ba206d5352c6e2f63
karnikamit <amit.karnik@edgenetworks.in>
2015-09-13 09:24:52 +0530

    Understanding DP prob1 Fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -9,4 +9,21 @@ def fibo(n):
     else:
         return fibo(n-1) + fibo(n-2)
 
-print fibo(10)  #55
+print fibo(10)  # 55
+
+"""
+Reducing the complexity from exponential to polynomial using Memorization
+"""
+
+
+fibTable = {1: 1, 2: 1}
+
+
+def Fibo(n):
+    if n in fibTable:
+        return fibTable[n]
+    else:
+        fibTable[n] = Fibo(n-1) + Fibo(n-2)
+        return fibTable[n]
+
+print Fibo(10)
