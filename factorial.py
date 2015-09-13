e975a10c993ba023c91abc7d6a3975aa505816d0
karnikamit <amit.karnik@edgenetworks.in>
2015-09-13 10:18:18 +0530

    Understanding DP prob1 Fibonacci

---
+++ factorial.py
@@ -0,0 +1,24 @@
+__author__ = 'karnikamit'
+
+# Finding the factorial of a given number
+
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n-1)
+print fact(10)
+
+# Using table
+factTable = {}
+def factorial(n):
+    try:
+        return factTable[n]
+    except KeyError:
+        if n == 0:
+            factTable[n] = 1
+            return 1
+        else:
+            factTable[n] = n * factorial(n-1)
+        return factTable[n]
+print factorial(10)
